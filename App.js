832ef57977d0be17bbe492e7836ba57521b40bff
xavier <omondix214@gmail.com>
2023-05-24 17:27:56 +0300

    initial

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import OneSignal from "react-native-onesignal";
+import Constants from "expo-constants";
+OneSignal.setAppId(Constants.manifest.extra.oneSignalAppId);
+OneSignal.setAppId("43ad2f5a-8798-4773-bc8b-6ed3de961eca");
 
 export default function App() {
   return (
@@ -13,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
